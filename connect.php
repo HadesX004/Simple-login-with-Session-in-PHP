117fe99d867f9941df65549f94f791caec42ec0c
Hades <86209431+HadesX004@users.noreply.github.com>
2023-08-12 12:40:41 +0000

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
     try{
-        $pdo = new PDO("mysql:host=127.0.0.1:3306;dbname=bd_projeto", "root", "");
+        $pdo = new PDO("mysql:host=127.0.0.1:3306;dbname=bd_projeto", "", "");
 
     }catch (PDOException $e){
         echo("<script>window.location.assign('failed.php');</script>");
